8a4093040c19d0d9fe5d0ad3149b54f8c7e8c9ba
p2svn <33637233+p2svn@users.noreply.github.com>
2017-12-13 12:54:03 +0700

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,8 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "nccoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001ffffffffffff01029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee49
-4ffbbd088071210143788b16f108429d18ae41b6eaf931b6299ac539594a6009b86b6d8734e4f1d7";
+const char     GENESIS_COINBASE_TX_HEX[]                     ="013c01ff0001ffffffffffff01029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210143788b16f108429d18ae41b6eaf931b6299ac539594a6009b86b6d8734e4f1d7";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
